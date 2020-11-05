6cca56c587a577579fb6527272bdb7bebd8454b9
Alexey Vlasov <gdevalex@gmail.com>
2020-11-05 23:24:17 +0500

    Save a try to load nuitrack library (no jni)

---
+++ NuitrackJavaWrapper/src/main/java/NuitrackJavaWrapper/TestMain.java
@@ -0,0 +1,8 @@
+package NuitrackJavaWrapper;
+
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        NuitrackImport nuitrack = new NuitrackImport();
+    }
+}
